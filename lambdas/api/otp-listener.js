deee08af2af5728e48c034ca6a27b55c54aaac16
jordmax12 <jordmax12@gmail.com>
2022-07-12 16:42:30 -0500

    added comment

--- lambdas/api/otp-listener.js
+++ lambdas/api/otp-listener.js
@@ -1,5 +1,6 @@
 const { returnHelper } = require('./helpers/response');
 
+// TODO: if no new image, does this lambda trigger? if so we need to handle this.
 module.exports.handler = async (event) => {
   console.log(JSON.stringify(event, null, 4));
   return returnHelper(200, { success: true });
