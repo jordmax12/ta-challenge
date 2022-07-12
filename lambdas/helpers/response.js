7899e1fed5407fc302d0b4976d5fbf261a54a457
jordmax12 <jordmax12@gmail.com>
2022-07-12 18:32:33 -0500

    added some documentation

--- lambdas/helpers/response.js
+++ lambdas/helpers/response.js
@@ -1,3 +1,9 @@
+/**
+ * Helper function to create a formatted response object.
+ * @param {Int} statusCode HTTP Status Code.
+ * @param {Object} body Body you want to send in the response.
+ * @returns Formatted response object.
+ */
 const returnHelper = (statusCode = 200, body = {}) => ({
   statusCode,
   body: JSON.stringify(body),
