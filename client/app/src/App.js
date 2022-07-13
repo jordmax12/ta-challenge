300b1756b2701b7fc31e8d2f8b90f1d977c5b7d5
jordmax12 <jordmax12@gmail.com>
2022-07-13 00:09:32 -0500

    removed commented code

--- client/app/src/App.js
+++ client/app/src/App.js
@@ -9,18 +9,6 @@ function App() {
         <CookiesProvider>
             <div className="App" style={{ height: '100vh' }}>
                 <Home isLoggedIn={cookies.isLoggedIn} />
-                {/* <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>Thoughtful Automation</p>
-                    <a
-                        className="App-link"
-                        href="https://www.youtube.com/watch?v=D5_FHbdsjRc&list=PLmexTtcbIn_gP8bpsUsHfv-58KsKPsGEo"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn Serverless
-                    </a>
-                </header> */}
             </div>
         </CookiesProvider>
     );
